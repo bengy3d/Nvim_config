2df212ea2f360d7bf15ebba586f891923472e1f4
Beniamin Pel <bengy3d@gmail.com>
2023-04-01 14:23:09 +0200

    Followed the Primeagen nvim config tutorial

---
+++ lua/ben/remap.lua
@@ -0,0 +1,23 @@
+vim.g.mapleader = " "
+
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Paste and replace and keep original value
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- Clipboard integration
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+vim.keymap.set("n", "<leader>d", "\"_d")
+vim.keymap.set("v", "<leader>d", "\"_d")
+
+vim.keymap.set("n", "Q", "<nop>")
+
