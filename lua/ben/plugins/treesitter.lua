6726443b60f2900ef1fc698feb7b3d676943e37e
Bengy <bengy3d@gmail.com>
2026-05-16 12:17:37 +0200

    Fixed treesitter after update

--- lua/ben/plugins/treesitter.lua
+++ lua/ben/plugins/treesitter.lua
@@ -1,35 +1,20 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
+        lazy = false,
         build = ":TSUpdate",
         config = function()
-            local configs = require("nvim-treesitter.configs").setup{
-                -- A list of parser names, or "all" (the five listed parsers should always be installed)
-                ensure_installed = { "javascript", "java", "kotlin", "typescript", "c", "lua", "vim", "query"},
+            require("nvim-treesitter").setup()
 
-                -- Install parsers synchronously (only applied to `ensure_installed`)
-                sync_install = false,
+            require("nvim-treesitter").install({
+                "javascript", "java", "kotlin", "typescript", "c", "lua", "vim", "query"
+            })
 
-                -- Automatically install missing parsers when entering buffer
-                -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-                auto_install = true,
-
-                indent = {
-                    enable = true,
-                    disable = {'python'},
-                },
-
-
-                highlight = {
-                    enable = true,
-
-                    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-                    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                    -- Instead of true it can also be a list of languages
-                    additional_vim_regex_highlighting = false,
-                },
-            }
+            vim.api.nvim_create_autocmd("FileType", {
+                callback = function(args)
+                    pcall(vim.treesitter.start, args.buf)
+                end,
+            })
         end,
     },
 }
